2970081ae01396478686077cd50b5749767dacdd
moizsheraz <moiz77131@gmail.com>
2023-11-09 20:33:28 +0500

    UI

--- client/src/Pages/CreatePost.jsx
+++ client/src/Pages/CreatePost.jsx
@@ -1,8 +1,53 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import ReactQuill from "react-quill";
+import EditorToolbar, { modules, formats } from "../Componets/PostToolbar"
+import "react-quill/dist/quill.snow.css";
 const CreatePost = () => {
+
+  const [state, setState] = useState({ value: null });
+  const handleChange = value => {
+    setState({ value });
+    console.log(state)
+  };
   return (
-    <div>CreatePost</div>
+    <>
+
+    <div className="container mx-auto p-4">
+      <div className="bg-white p-8 rounded-lg shadow-md">
+        <h2 className="text-2xl font-semibold mb-6">Create a Post</h2>
+
+        <form>
+          <div className="mb-4 relative">
+            <label htmlFor="postContent" className="block  text-gray-600 font-bold text-xl m-2">Post Content</label>
+            <EditorToolbar />
+             <ReactQuill
+        theme="snow"
+        value={state.value}
+        onChange={handleChange}
+        placeholder={"Create Post..."}
+        modules={modules}
+        formats={formats}
+      />
+         
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-600 mb-2">Add Media (Optional)</label>
+            <input type="file" name="media" className="border-gray-300 rounded-md" />
+          </div>
+
+          <div className="text-right">
+            <button type="submit" className="bg-gray-500 text-white px-4 py-2 rounded-md">
+              Post
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+    {/* Post Preview */}
+    <div>
+    </div>
+        </>
   )
 }
 
--- client/src/Pages/UserProfile.jsx
+++ client/src/Pages/UserProfile.jsx
@@ -2,7 +2,45 @@ import React from 'react'
 
 const UserProfile = () => {
   return (
-    <div>UserProfile</div>
+    <div class="container mx-auto p-4">
+    <div class="bg-white p-8 rounded-lg shadow-md">
+        <div class="flex justify-center mb-4">
+            <img src="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Profile Picture" class="w-20 h-20 rounded-full"/>
+        </div>
+        
+        <div class="text-center mb-4">
+            <h2 class="text-xl font-semibold">Your Name</h2>
+            <p class="text-gray-500">@username</p>
+        </div>
+
+        <div class="text-center mb-6">
+            <button class="bg-gray-500 text-white px-4 py-2 rounded-xl">Edit Profile</button>
+        </div>
+
+        <div class="flex justify-around text-gray-600">
+            <div>
+                <p class="text-lg font-semibold">500</p>
+                <p>Posts</p>
+            </div>
+            <div>
+                <p class="text-lg font-semibold">1.5k</p>
+                <p>Followers</p>
+            </div>
+            <div>
+                <p class="text-lg font-semibold">800</p>
+                <p>Following</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="mt-8">
+        <div class="bg-white p-4 mb-4 rounded-lg shadow-md">
+            <p>This is a sample post. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+
+
+    </div>
+</div>
   )
 }
 
